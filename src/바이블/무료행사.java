9f794f41048fb1c4c4ddbe80cf67f2ea93e52660
Jeong Seok Jin <52066509+Jeongseokjin@users.noreply.github.com>
2025-12-30 18:33:54 +0900

    [바이블] 01 - 무료행사

---
+++ src/바이블/무료행사.java
@@ -0,0 +1,46 @@
+package 바이블;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.HashSet;
+import java.util.Set;
+import java.util.StringTokenizer;
+
+public class 무료행사 {
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        StringTokenizer st;
+
+
+        //입력받기
+
+        st = new StringTokenizer(br.readLine());
+        int n = Integer.parseInt(st.nextToken());
+        int target = Integer.parseInt(st.nextToken());
+        st = new StringTokenizer(br.readLine());
+        int[] prices = new int[n];
+        for (int i = 0; i < n; i++) {
+            prices[i] = Integer.parseInt(st.nextToken());
+        }
+
+        Set<Integer> set = new HashSet<>();
+        int count = 0;
+        for (int price : prices) {
+            int need = target - price;
+            if (set.contains(need)) {
+                count++;
+            }
+
+            set.add(price);
+
+        }
+        System.out.println(count);
+
+
+    }
+
+
+}
